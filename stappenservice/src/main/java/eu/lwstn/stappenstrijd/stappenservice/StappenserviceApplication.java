2fd0479d1f79f229e23135fbf5bb33ee57c60328
Jeroen Leeuwestein <jeroen@lwstn.eu>
2021-01-31 12:42:54 +0100

    Add stuff generated by initializr

---
+++ stappenservice/src/main/java/eu/lwstn/stappenstrijd/stappenservice/StappenserviceApplication.java
@@ -0,0 +1,13 @@
+package eu.lwstn.stappenstrijd.stappenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StappenserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StappenserviceApplication.class, args);
+	}
+
+}
---
+++ stappenservice/src/test/java/eu/lwstn/stappenstrijd/stappenservice/StappenserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.lwstn.stappenstrijd.stappenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StappenserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
